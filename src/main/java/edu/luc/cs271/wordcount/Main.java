e83804e82c58241e5e2ff4a92e3e2f0888aa9a7c
Ethan M <emohan@luc.edu>
2017-11-08 03:22:22 +0000

    got tests down, passing, working on other funtional requirments

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -1,52 +1,39 @@
 package edu.luc.cs271.wordcount;
 
+import java.io.*;
 import java.util.*;
 import java.util.Scanner;
-import java.io.*;
 
 public class Main {
 
   public static void main(final String[] args) throws InterruptedException {
-    
-        // set up the scanner so that it separates words based on space and punctuation
+
+    // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
-    //final Scanner input = new File(input.nextLine());
-    //input = new Scanner(file);
-
-
+    // final Scanner input = new File(input.nextLine());
+    // input = new Scanner(file);
 
     // DONE?TODO complete this main program
     // 1. create a WordCounter instance
     Map<String, Integer> newMap = new HashMap<>();
-    WordCounter wordy = new WordCounter(newMap); 
+    WordCounter wordy = new WordCounter(newMap);
     // 2. use this to count the words in the input
     wordy.countWords(input);
     // 3. determine the size of the resulting map
-      int size = newMap.size();
+    int size = newMap.size();
     // 4. create an ArrayList of that size and
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
-        List<Map.Entry<String, Integer>> list = 
-    new ArrayList<Map.Entry<String, Integer>>(newMap.entrySet());
+    List<Map.Entry<String, Integer>> list =
+        new ArrayList<Map.Entry<String, Integer>>(newMap.entrySet());
     // 6. sort the ArrayList in descending order by count
     //    using Arrays.sort and an instance of the provided comparator (after fixing the latter)
-    Map<String, List<String>> contacts = new TreeMap<String,  List<String>>();
+    Map<String, List<String>> contacts = new TreeMap<String, List<String>>();
 
-    //ArrayList arrraylist = Arrays.sort(DescendingByCount);
+    // ArrayList arrraylist = Arrays.sort(DescendingByCount);
     Collections.sort(list, new DescendingByCount());
     // 7. print the (up to) ten most frequent words in the text
-    for(int i=0;i<words.length;i++)
-{
-     String s = nextLine[i];
-     if(words_count.keySet().contains(s))
-     {
-          Integer count = words_count.get(s) + 1;
-          words_count.put(s, count);
-     }
-     else
-          words_count.put(s, 1);
-}
-    
-
+    for (int i = 0; i < 11; i++) {
+      System.out.println(list.get(i));
+    }
   }
 }
-
--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -2,52 +2,48 @@ package edu.luc.cs271.wordcount;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestWordCounter {
 
-  // done complete this test class
+  // Done complete this test class
 
-  // done declare a reference to the SUT (system under test), i.e., WordCounter
-
-  
-  Map<String, Integer> zMap = new HashMap<>();
-  WordCounter count = new WordCounter(zMap); 
+  // Done declare a reference to the SUT (system under test), i.e., Wordwordy
+  WordCounter wordy;
 
   @Before
   public void setUp() {
-    // done create the SUT instance
-    this.count = count;
+    // Done create the SUT instance
+    Map<String, Integer> map = new HashMap<String, Integer>();
+    wordy = new WordCounter(map);
   }
 
   @After
   public void tearDown() {
-    // done  set the SUT instance to null
-    count = null;
-    
+    // Done set the SUT instance to null
+    wordy = null;
   }
 
   @Test
   public void testGetCountEmpty() {
 
-    // done verify that the SUT initially returns an empty map
-    assertTrue(zMap.isEmpty)
+    // Done verify that the SUT initially returns an empty map
+    assertTrue(wordy.getCounts().isEmpty());
   }
 
   @Test
   public void testGetCountNonEmpty() {
-    int correctCounts = WordCounter.getCount("the");
-    assertEquals(correctCounts, 15);
-    int correctCountsTwo = WordCounter.getCount("and");
-    asssertEquals(correctCountsTwo, 12);
-    int correctCountsThree = WordCounter.getCount("qwerty");
-    asssertEquals(correctCountsThree, 15);
-    int correctCountsFour = WordCounter.getCount("poop");
-    asssertEquals(correctCountsFour, 0);
 
+    // Done run the SUT on a specific String iterator with some repeated words,
+    // then use assertions to verify the correct counts
+    // do this for at least two words in the iterator and two not in the iterator
 
+    wordy.countWords(Arrays.asList("HELLO", "OK", "JUST", "WORK").iterator());
 
+    assertEquals(wordy.getCount("HELLO"), 1);
+    assertEquals(wordy.getCount("OK"), 1);
   }
 }

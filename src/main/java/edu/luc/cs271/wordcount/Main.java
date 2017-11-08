220de297fae721b330dcf1670eb48d42cf89ceca
Ethan M <emohan@luc.edu>
2017-11-08 01:51:46 +0000

    added map<string, int> to list

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -10,7 +10,6 @@ public class Main {
     
         // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
-    String[] words = input.split(" ");
     //final Scanner input = new File(input.nextLine());
     //input = new Scanner(file);
 
@@ -23,18 +22,17 @@ public class Main {
     // 2. use this to count the words in the input
     wordy.countWords(input);
     // 3. determine the size of the resulting map
-      int size = newMap.values().size();
-
+      int size = newMap.size();
     // 4. create an ArrayList of that size and
-    ArrayList sizes = new ArrayList(size);
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
-        List<newMap.Entry<String, Integer>> entries = new ArrayList<>(size);
-    entries.addAll(wordy.values().entrySet());
+        List<Map.Entry<String, Integer>> list = 
+    new ArrayList<Map.Entry<String, Integer>>(newMap.entrySet());
     // 6. sort the ArrayList in descending order by count
     //    using Arrays.sort and an instance of the provided comparator (after fixing the latter)
     Map<String, List<String>> contacts = new TreeMap<String,  List<String>>();
 
     //ArrayList arrraylist = Arrays.sort(DescendingByCount);
+    Collections.sort(list, new DescendingByCount());
     // 7. print the (up to) ten most frequent words in the text
     for(int i=0;i<words.length;i++)
 {
